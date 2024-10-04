57072aa9ec5b9f862ae3e00aa497cf9ad9ee1890
rodorigo15700 <164518665+rodorigo15700@users.noreply.github.com>
2024-10-04 11:31:42 -0300

    trabalho
    trabalho

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'programaçao',
+    'oque e programaçao?',
+    'Programação é o processo de criação de um conjunto de instruções que dizem ao computador como realizar uma tarefa. Podemos programar usando uma variedade de linguagens de programação de computadores, como JavaScript, Python e C++.'
+)
+
+criaCartao(
+    ' html',
+    'O que é html?',
+    'TML (Linguagem de Marcação de HiperTexto) é o bloco de construção mais básico da web. Define o significado e a estrutura do conteúdo da web. Outras tecnologias além do HTML geralmente são usadas para descrever a aparência/apresentação (CSS) ou a funcionalidade/comportamento (JavaScript) de uma página da web'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Azul em ingles?',
+    'Blue'
+)
